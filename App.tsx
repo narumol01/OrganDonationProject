5262c2fddea7e6e45cc95b85bfc3f7ab43b4e3a8
narumol01 <73689289+narumol01@users.noreply.github.com>
2023-03-24 15:41:53 +0700

    test2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>พี่เฟรมคนเท่</Text>
+      <Text>พี่เฟรมคนเท่ และพี่เปาคนหล่อ</Text>
       <StatusBar style="auto" />
     </View>
   );
