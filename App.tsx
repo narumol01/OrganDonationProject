484945d3b05a1ea7b1cee2a680a46d8700c9bb8d
Kulanit63070011 <73680667+Kulanit63070011@users.noreply.github.com>
2023-04-03 23:11:37 +0700

    frame's first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FormRequestOrganScreen from './frontend/screens/FormRequestOrganScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>พี่เฟรมคนเท่ และพี่เปาคนหล่อ makmak</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>พี่เฟรมคนเท่ และพี่เปาคนหล่อ makmak</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <FormRequestOrganScreen></FormRequestOrganScreen>
   );
 }
 
